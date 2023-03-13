089e4c4f595cfaec549cdf402a5fa6454bb0f397
Petr Nymsa <nymsa.petr@outlook.cz>
2023-03-13 13:29:47 +0100

    Change builder extension to .mapper.g.dart

--- packages/auto_mapper/lib/builder.dart
+++ packages/auto_mapper/lib/builder.dart
@@ -7,6 +7,6 @@ import 'package:source_gen/source_gen.dart';
 /// Main Builder for the [Mapping] Annotation
 Builder automapperBuilder(BuilderOptions options) => PartBuilder(
       [AutoMapperGenerator()],
-      '.g.dart',
+      '.mapper.g.dart',
       options: options,
     );
