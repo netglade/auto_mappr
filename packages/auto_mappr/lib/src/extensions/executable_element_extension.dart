badcdaca80e38d4135b2a71077bbbc094645fc63
Nicolas Schlecker <nicolasschlecker@gmail.com>
2023-04-28 16:50:29 +0200

    fix formatting

--- packages/auto_mappr/lib/src/extensions/executable_element_extension.dart
+++ packages/auto_mappr/lib/src/extensions/executable_element_extension.dart
@@ -1,12 +1,11 @@
 import 'package:analyzer/dart/element/element.dart';
 
 extension ExecutableElementExtension on ExecutableElement {
-          // For top-level functions `isStatic` is true
-          // but the enclosing element has no display name.
-          // This could result in the expression being `.someFunction()` which is invalid.
-          // Therefore, we only add the enclosing element's display name if it is not empty.
-  String get referCallString => isStatic &&
-          enclosingElement.displayName != ''
+  // For top-level functions `isStatic` is true
+  // but the enclosing element has no display name.
+  // This could result in the expression being `.someFunction()` which is invalid.
+  // Therefore, we only add the enclosing element's display name if it is not empty.
+  String get referCallString => isStatic && enclosingElement.displayName != ''
       ? '${enclosingElement.displayName}.$displayName'
       : displayName;
 }
